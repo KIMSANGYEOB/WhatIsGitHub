2287b0f219ae228d4ce3ed3846c3451944ff1b2e
KIMSANGYEOB <sykim10070@gmail.com>
2018-10-30 20:36:20 +0900

    script 내용 수정

--- kakaoLogin/script.js
+++ kakaoLogin/script.js
@@ -1,6 +1,6 @@
 (function(){
 
-//주석을 답시다.
+//주석을 답시다.gkgkgkgk
 //김상엽 천재라 이미지 오류 수정했음
 const ryan = document.querySelector('#ryan');
 const face = document.querySelectorAll('.ears, .eyes, .muzzle');
